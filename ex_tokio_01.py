24313276734e3e092573646c931e10b7b8f60d7d
bressar <dougbressar@gmail.com>
2024-07-02 13:54:26 +0100

    Update ex_tokio_01.py

--- ex_tokio_01.py
+++ ex_tokio_01.py
@@ -5,6 +5,7 @@ media = (n1 + n2 + n3) / 3
 print(f'"A nota média é {media}"')
 
 
+print('\n')  
 """ 2) Continuar o exercício anterior. Cada nota tem o valor percentual seguinte:
 1ª nota: vale 15% do total
 2ª nota: vale 35% do total
@@ -17,6 +18,8 @@ nota_3 = 4
 media_notas = (nota_1 * 0.15) + (nota_2 * 0.35) + (nota_3 * 0.5)
 print(f"Média Final: {media_notas:.2f}")
 
+
+print('\n')  
 """ 3) Ler 2 números inteiros por teclado e avaliar de forma booleana (True ou False) os seguintes aspetos:
 Se os dois números são iguais
 Se os dois números são diferentes
@@ -37,4 +40,31 @@ if (num1 > num2):
 
 if (num2 >= num1):
     print(f"O 2° número: {num2} é maior ou igual do que o 1° número: {num1}")
+
+  
+print('\n')    
+""" 4) Ler uma cadeia de texto introduzida pelo teclado e avaliar se tem um comprimento maior ou igual a três e menor que dez (mostrar unicamente True ou False) Pista: utilizar operadores lógicos """
+
+frase = str(input("Escreva uma frase para saber se o seu\n"
+                  "número de caracteres é maior ou igual a 3\n"
+                  "ou menor do que 10: "))
+
+if ((len(frase) >= 3) and (len(frase) < 10)):
+    print(True)
+else:
+    print(False)
+    
+
+print('\n')  
+""" 5) Transcrever o seguinte algoritmo (utilizar operadores de atribuição sempre que possa):
+Guardar numa variável o número_mágico o valor 12345679 (sem o 8)
+Ler por ecrã outro número_utilizador, especificar que seja entre 1 e 9 (assegurar que seja inteiro)
+Multiplicar o número_utilizador por 9 por si mesmo
+Multiplicar o número_mágico pelo número_utilizador por si mesmo
+Finalmente mostrar o valor final do número_mágico por ecrã """
+
+num_magic = int(12345679)
+num_utl = int(input("Digite um número entre 1 e 9: "))
+num_utl *= 9
+print(num_utl * num_magic)
 
