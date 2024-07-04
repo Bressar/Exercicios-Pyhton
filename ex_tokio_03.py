abf870167aa36409d65fb8439df7d51c49d111a7
bressar <dougbressar@gmail.com>
2024-07-04 17:23:30 +0100

    Update ex_tokio_03.py

--- ex_tokio_03.py
+++ ex_tokio_03.py
@@ -59,3 +59,56 @@ print(musics)# Com a função clear() já não fica nenhuma informação da vari
 print("\n",artists)
 # Como a tupla é uma CONSTANTE, a função clear ou delete não funcionam com ela...
 # se ouver um outro jeito de apagar, além de apagar manualmente a variável eu gosaria de saber.. tks
+
+""" 
+2) Trabalhar com sets e dicionários
+
+Criar um set e um dicionário que contenham strings (pelo menos 3 elementos no caso do set e 3 conjuntos de chave: valor no caso do dicionário). Temática livre, sobre o que quiser, veículos, comida, música, etc.
+Mostrar o set e o dicionário
+Mostrar (se puder) o 2º elemento do set e o valor da primeira chave-valor do dicionário
+Modificar (se puder) algum elemento do set ou do dicionário. E mostrar o resultado
+Mostrar o tamanho do set e do dicionário
+Fazer uma pesquisa de um elemento dentro do set e dentro do dicionário. Imprimir o resultado (True ou False).
+Adicionar (se puder) algum elemento ao set e alguma chave-valor ao dicionário. Imprimir novamente o set e o dicionário para verificar se a acção foi realizada corretamente.
+Apagar ou eliminar (se puder) o conteúdo do set e do dicionário. Imprimir de novo o set e o dicionário para verificar se a acção foi realizada corretamente. """
+
+# Criação
+animais = {'aves', 'peixes', 'reptéis', 'anfíbios'} # set
+
+mamiferos = {
+    'classe' : 'felino',
+    'tipo' : 'gato',
+    'raça' : 'persa',
+    'nome' : 'pepi'
+}
+# Mostrar
+print("\nLista de animais:", animais)
+print("\nDicionário de mamíferos:", mamiferos)
+
+# Mostrar elementos específicos
+# Só é possível usar o index após converter o set p/ uma lista
+# Já que o set é um conjunto desordenado, assim como os dicionários
+animais_list = list(animais)# escolhe os itens para a lista de forma aleatória
+print(animais_list)
+print("\n2° elemento do Set 'animais':",animais_list[1])
+
+mamiferos_keys = list(mamiferos)# tb poderia ser a função keys()
+mamiferos_values = mamiferos.values()
+mamiferos_keys = list(mamiferos_keys)
+mamiferos_values = list(mamiferos_values)
+print("\nPrimeira chave/valor do diconário:", mamiferos_keys[0], ":", mamiferos_values[0])
+
+ # Modificar
+animais.remove("peixes")
+animais.add("roedores")
+print("\nNo Set foi removido o item 'peixes' e acrescentado o item 'roedores':", animais)
+
+mamiferos["nome"] = "blau-blau"
+print("\nNo dicionário foi mudada o nome:", mamiferos)
+
+# Tamanho
+print("\nO Set animais tem:", len(animais), "elementos")
+print("\nO dicionário mamiferos tem:", len(mamiferos), "elementos")
+
+
+
